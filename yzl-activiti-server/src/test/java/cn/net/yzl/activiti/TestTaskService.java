9eb2cf33d5153e1ce5dcd85cdd328ea02b19da8c
yuhongbo <yuhongbo@yuzhilin.net.cn>
2021-03-11 22:07:02 +0800

    测试TaskService

--- yzl-activiti-server/src/test/java/cn/net/yzl/activiti/TestTaskService.java
+++ yzl-activiti-server/src/test/java/cn/net/yzl/activiti/TestTaskService.java
@@ -35,8 +35,7 @@ public class TestTaskService {
     private SecurityUtil securityUtil;
 
     /**
-     * 查看流程定义内容
-     * Activiti7可以自动部署流程
+     * 查看任务详情
      */
     @Test
     public void findPersonnelTaskList() {
@@ -61,47 +60,163 @@ public class TestTaskService {
 
     }
 
-        /**
-         * 测试完成个人任务
-         */
-        @Test
-        public void completTask(){
-            //获取引擎
-            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
-            //获取操作任务的服务 TaskService
-            TaskService taskService = processEngine.getTaskService();
-            //完成任务，参数 流程实例id 完成的任务
-            Task task = taskService.createTaskQuery().processDefinitionId("15001").taskAssignee("rose").singleResult();
-            log.info("流程实例id：{}",task.getProcessInstanceId());
-            log.info("任务id：{}",task.getId());
-            log.info("任务负责人：{}",task.getAssignee());
-            log.info("任务名称:{}",task.getName());
-            taskService.complete(task.getId());
-
+    /**
+     * 测试完成个人任务
+    */
+    @Test
+    public void completTask(){
+        //获取引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //获取操作任务的服务 TaskService
+        TaskService taskService = processEngine.getTaskService();
+        //完成任务，参数 流程实例id 完成的任务
+        Task task = taskService.createTaskQuery().processDefinitionId("15001").taskAssignee("rose").singleResult();
+        log.info("流程实例id：{}",task.getProcessInstanceId());
+        log.info("任务id：{}",task.getId());
+        log.info("任务负责人：{}",task.getAssignee());
+        log.info("任务名称:{}",task.getName());
+        taskService.complete(task.getId());
         }
-        @Test
-        public void findProcessInstance(){
-        //获取processEngine
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine(); //  获取TaskService
-        TaskService taskService = processEngine.getTaskService(); //  获取RuntimeService
-        RuntimeService runtimeService = processEngine.getRuntimeService(); //  查询流程定义的对象
-        Task task = taskService.createTaskQuery()
-        .processDefinitionKey("myEvection1")
-        .taskAssignee("张三")
-        .singleResult();
-        //使用task对象获取实例id
-        String processInstanceId = task.getProcessInstanceId();
-        //使用实例id，获取流程实例对象
-        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery()
-        .processInstanceId(processInstanceId)
-        .singleResult();
-        //使用processInstance，得到 businessKey
-        String businessKey = processInstance.getBusinessKey();
-        System.out.println("businessKey=="+businessKey);
+
+
+    //根据候选人查询组任务
+    @Test
+    public void findGroupTaskList() {
+        // 流程定义key
+        String processDefinitionKey = "evection3";
+        // 任务候选人
+        String candidateUser = "lisi";
+        // 获取processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 创建TaskService
+        TaskService taskService = processEngine.getTaskService();
+        //查询组任务
+        List<Task> list = taskService.createTaskQuery()
+        .processDefinitionKey(processDefinitionKey)
+        .taskCandidateUser(candidateUser)//根据候选人查询
+        .list();
+        for (Task task : list) {
+        System.out.println("----------------------------");
+        System.out.println("流程实例id：" + task.getProcessInstanceId());
+        System.out.println("任务id：" + task.getId());
+        System.out.println("任务负责人：" + task.getAssignee());
+        System.out.println("任务名称：" + task.getName());
+    }
 
   }
 
+    //拾取组任务 候选人员拾取组任务后该任务变为自己的个人任务
+    @Test
+    public void claimTask(){
+        // 获取processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        //要拾取的任务id
+        String taskId = "6302";
+        //任务候选人id
+        String userId = "lisi";
+        //拾取任务
+        //即使该用户不是候选人也能拾取(建议拾取时校验是否有资格)  
+        //校验该用户有没有拾取任务的资格
+        Task task = taskService.createTaskQuery()
+        .taskId(taskId)
+        .taskCandidateUser(userId)//根据候选人查询
+        .singleResult();
+        if(task!=null){
+        //拾取任务
+        taskService.claim(taskId, userId);
+        System.out.println("任务拾取成功");
+        }
+}
+
+    //查询个人待办任务 查询方式同个人任务查询
+    @Test
+    public void findPersonalTaskList() {
+        // 流程定义key
+        String processDefinitionKey = "evection1";
+        // 任务负责人
+        String assignee = "zhangsan";
+        // 获取processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 创建TaskService
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> list = taskService.createTaskQuery().processDefinitionKey(processDefinitionKey)
+        .taskAssignee(assignee)
+        .list();
+        for (Task task : list) {
+        System.out.println("----------------------------");
+        System.out.println("流程实例id：" + task.getProcessInstanceId());
+        System.out.println("任务id：" + task.getId());
+        System.out.println("任务负责人：" + task.getAssignee());
+        System.out.println("任务名称：" + task.getName());
+        }
     }
+
+    //办理个人任务 同个人任务办理
+    /*完成任务*/
+    @Test
+    public void completeTask(){
+        // 任务ID
+        String taskId = "12304";
+        // 获取processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        processEngine.getTaskService()
+        .complete(taskId);
+        System.out.println("完成任务："+taskId);
+    }
+
+
+    /*
+     *归还组任务，由个人任务变为组任务，还可以进行任务交接
+     */
+    @Test
+    public void setAssigneeToGroupTask() {
+        // 获取processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 查询任务使用TaskService
+        TaskService taskService = processEngine.getTaskService();
+        // 当前待办任务
+        String taskId = "6004";
+        // 任务负责人
+        String userId = "zhangsan2";
+        // 校验userId是否是taskId的负责人，如果是负责人才可以归还组任务
+        Task task = taskService
+        .createTaskQuery()
+        .taskId(taskId)
+        .taskAssignee(userId)
+        .singleResult();
+        if (task != null) {
+        // 如果设置为null，归还组任务,该 任务没有负责人
+        taskService.setAssignee(taskId, null);
+        }
+    }
+
+
+    //任务交接,任务负责人将任务交给其它候选人办理该任务
+    @Test
+    public void setAssigneeToCandidateUser() {
+        // 获取processEngine
+                ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 查询任务使用TaskService
+        TaskService taskService = processEngine.getTaskService();
+        // 当前待办任务
+        String taskId = "6004";
+        // 任务负责人
+        String userId = "zhangsan2";
+        // 将此任务交给其它候选人办理该 任务
+        String candidateuser = "zhangsan";
+        // 校验userId是否是taskId的负责人，如果是负责人才可以归还组任务
+        Task task = taskService
+        .createTaskQuery()
+        .taskId(taskId)
+        .taskAssignee(userId)
+        .singleResult();
+        if (task != null) {
+        taskService.setAssignee(taskId, candidateuser);
+        }
+    }
+
+}
 
 
 
