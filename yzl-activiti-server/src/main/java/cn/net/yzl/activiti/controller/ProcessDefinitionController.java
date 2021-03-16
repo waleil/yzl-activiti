d484c061ba9a8cfad7a0abc2ccf63b0cb5a7c15c
limisheng <limisheng@yuzhilin.net.cn>
2021-03-16 21:33:29 +0800

    activiti 接口描述

--- yzl-activiti-server/src/main/java/cn/net/yzl/activiti/controller/ProcessDefinitionController.java
+++ yzl-activiti-server/src/main/java/cn/net/yzl/activiti/controller/ProcessDefinitionController.java
@@ -4,6 +4,11 @@ import cn.net.yzl.activiti.model.dto.ProcessDefinitionDTO;
 import cn.net.yzl.activiti.model.vo.CreateProcessVO;
 import cn.net.yzl.activiti.service.IProcessDefinitionService;
 import cn.net.yzl.common.entity.ComResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +18,8 @@ import java.util.List;
 /**
  * 流程管理控制层
  */
+@Api(tags = "activiti对应流程实例接口")
+@Slf4j
 @RestController
 public class ProcessDefinitionController {
 
@@ -23,6 +30,7 @@ public class ProcessDefinitionController {
      * 查询流程列表
      * @return
      */
+    @ApiOperation(value = "查询流程列表", notes = "查询流程列表")
     @GetMapping(value = "/process/definitions")
     public ComResponse<List<ProcessDefinitionDTO>> getProcessDefinitionList() {
         return processDefinitionService.getProcessDefinitionList();
@@ -32,6 +40,7 @@ public class ProcessDefinitionController {
      * 创建流程
      * @return
      */
+    @ApiOperation(value = "创建流程", notes = "创建流程")
     @PostMapping(value = "/process")
     public ComResponse fileUpload(@RequestParam("multipartFile") MultipartFile multipartFile, CreateProcessVO createProcessVO) {
         return processDefinitionService.fileUpload(multipartFile, createProcessVO);
@@ -41,6 +50,10 @@ public class ProcessDefinitionController {
      * 删除流程
      * @return
      */
+    @ApiOperation(value = "删除流程", notes = "删除流程")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", paramType = "query", dataType = "String", required = true, value = "流程id"),
+    })
     @DeleteMapping("/{id}")
     @Deprecated
     public ComResponse delProcessDefinition(@PathVariable("id") String id) {
@@ -51,6 +64,10 @@ public class ProcessDefinitionController {
      * 流程发布
      * @return
      */
+    @ApiOperation(value = "流程发布", notes = "流程发布")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "fileId", paramType = "query", dataType = "String", required = true, value = "上传文件对应id"),
+    })
     @PostMapping(value = "/process/start/{fileId}")
     public ComResponse pushProcessDefinition(@PathVariable("fileId") Long fileId) {
         return processDefinitionService.pushProcessDefinition(fileId);
--- yzl-activiti-server/src/main/java/cn/net/yzl/activiti/controller/ProcessInstanceController.java
+++ yzl-activiti-server/src/main/java/cn/net/yzl/activiti/controller/ProcessInstanceController.java
@@ -1,17 +1,22 @@
 package cn.net.yzl.activiti.controller;
 
-import cn.net.yzl.activiti.model.vo.CreateProcessVO;
 import cn.net.yzl.activiti.service.IProcessInstanceService;
 import cn.net.yzl.common.entity.ComResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Api(tags = "activiti对应流程实例接口")
+@Slf4j
 @RestController
 public class ProcessInstanceController {
 
@@ -22,18 +27,29 @@ public class ProcessInstanceController {
      * 查询流程xml
      * @return
      */
+    @ApiOperation(value = "查询流程xml", notes = "查询流程xml")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "processId", paramType = "query", dataType = "String", required = true, value = "流程id"),
+            @ApiImplicitParam(name = "processName", paramType = "query", dataType = "String", required = true, value = "流程名称"),
+    })
     @GetMapping(value = "/process/xml/{processId}/{processName}")
     public void processXmlDetail(HttpServletResponse response, @PathVariable("processId") String processId,
                                      @PathVariable("processName") String processName) {
-        processInstanceService.processXmlDetail(response, processId, processName);
+       log.info("查询流程xml,入参processId：【{}】, processName:【{}】", processId, processName);
+       processInstanceService.processXmlDetail(response, processId, processName);
     }
 
     /**
      * 查看流程详情
      * @return
      */
+    @ApiOperation(value = "查看流程详情", notes = "查看流程详情")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "processId", paramType = "query", dataType = "String", required = true, value = "流程id"),
+    })
     @GetMapping(value = "/process/{processId}")
     public ComResponse processDetail(@PathVariable("processId") String processId) {
+        log.info("查看流程详情,processId：【{}】", processId);
         return processInstanceService.processDetail(processId);
     }
 
@@ -41,8 +57,13 @@ public class ProcessInstanceController {
      * 挂起流程实例
      * @return
      */
+    @ApiOperation(value = "挂起流程实例", notes = "挂起流程实例")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "processId", paramType = "query", dataType = "String", required = true, value = "流程id"),
+    })
     @GetMapping(value = "/process/suspend/{processId}")
     public ComResponse suspendProcessInstance(@RequestParam("processId") String processId) {
+        log.info("挂起流程实例,processId：【{}】", processId);
         return processInstanceService.suspendProcessInstance(processId);
     }
 
@@ -50,8 +71,13 @@ public class ProcessInstanceController {
      * 激活流程实例
      * @return
      */
+    @ApiOperation(value = "激活流程实例", notes = "激活流程实例")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "processId", paramType = "query", dataType = "String", required = true, value = "流程id"),
+    })
     @GetMapping(value = "/process/resume/{processId}")
     public ComResponse resumeProcessInstance(@RequestParam("processId") String processId) {
+        log.info("激活流程实例,processId：【{}】", processId);
         return processInstanceService.resumeProcessInstance(processId);
     }
 
